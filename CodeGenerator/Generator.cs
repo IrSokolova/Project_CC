d8cdbc3091e70c23de6d0ee28ee786dfa2650a2a
Alyona-art <alena.artemeva.2002.15@gmail.com>
2023-04-18 15:32:14 +0300

    +

--- CodeGenerator/Generator.cs
+++ CodeGenerator/Generator.cs
@@ -1,4 +1,7 @@
-﻿namespace DefaultNamespace.CodeGenerator;
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace DefaultNamespace.CodeGenerator;
 
 public class Generator
 {
@@ -6,5 +9,21 @@ public class Generator
     public Generator(Action action)
     {
         _action = action;
+        var name = new AssemblyNameDefinition("SuperGreeterBinary", new Version(1, 0, 0, 0));
+        var asm = AssemblyDefinition.CreateAssembly(name, "greeter.exe", ModuleKind.Console);
+
+        // импортируем в библиотеку типы string и void
+        asm.MainModule.Import(typeof(String));
+        var void_import = asm.MainModule.Import(typeof(void));
+
+        // создаем метод Main, статический, приватный, возвращающий void
+        var method = new MethodDefinition("Main", MethodAttributes.Static | MethodAttributes.Private | MethodAttributes.HideBySig, void_import);
+        // сохраняем короткую ссылку на генератор кода
+        var ip = method.Body.GetILProcessor();
+
+        // магия ленор!
+        ip.Emit(OpCodes.Ldstr, "Hello, ");
     }
+    
+    
 }
